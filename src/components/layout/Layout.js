7e9252ddde09e326c523de6e30cb20153ed5d04d
John_Lamy <johnclamy@gmail.com>
2021-03-07 22:47:39 +0000

    set layout css

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,11 +4,13 @@ import { Container, Col, Row } from 'react-bootstrap'
 import Navigation from './Navigation'
 import Footer from './Footer'
 
+import './Layout.css'
+
 export default function ({ children }) {
   return (
-    <Container fluid>
+    <Container className='Layout' fluid>
       <Row>
-        <Col md={{ span: 7, offset: 2 }}>
+        <Col md={{ span: 6, offset: 3 }}>
           <Navigation />
           <main>{children}</main>
           <Footer />
