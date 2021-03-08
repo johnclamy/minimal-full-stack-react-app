e3f3bf92173048755c07c83defb9c219e59d545c
John_Lamy <johnclamy@gmail.com>
2021-03-08 00:13:42 +0000

    setup  routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import Layout from './components/layout/Layout'
+import LandingPage from './components/pages/landing'
+import HomePage from './components/pages/home'
+import PetsPage from './components/pages/pets'
+import AboutPage from './components/pages/about'
+import * as ROUTES from './routes'
 
 export default function () {
   return (
     <Layout>
-      page content should go here...
+      <Switch>
+        <Route exact path={ROUTES.LANDING} component={LandingPage} />
+        <Route path={ROUTES.HOME} component={HomePage} />
+        <Route path={ROUTES.PETS} component={PetsPage} />
+        <Route path={ROUTES.ABOUT} component={AboutPage} />
+      </Switch>
     </Layout>
   )
 }
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,24 +1,33 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav } from 'react-bootstrap'
+
+import * as ROUTES from '../../routes/'
 
 export default function () {
   return (
-    <Navbar bg="light" expand="md">
-      <Navbar.Brand>
-        <img
-          width='30'
-          height='30'
-          alt='Tiny Pups logo'
-          src='/img/logo.png'
-          className='s-inline-block align-top'
-        />{' '}Tiny Pups
-      </Navbar.Brand>
+    <Navbar 
+      bg="light"
+      expand="md"
+      className='mb-3'
+    >
+      <LinkContainer to={ROUTES.LANDING}>  
+        <Navbar.Brand className="font-weight-bold text-muted">
+          <img
+            width='30'
+            height='30'
+            alt='Tiny Pups logo'
+            src='/img/logo.png'
+            className='s-inline-block align-top'
+          />{' '}Tiny Pups
+        </Navbar.Brand>
+      </LinkContainer>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
         <Nav>
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Pets</Nav.Link>
-          <Nav.Link href="#link">About</Nav.Link>
+          <LinkContainer to={ROUTES.HOME}><Nav.Link>Home</Nav.Link></LinkContainer>
+          <LinkContainer to={ROUTES.PETS}><Nav.Link>Pets</Nav.Link></LinkContainer>
+          <LinkContainer to={ROUTES.ABOUT}><Nav.Link>About</Nav.Link></LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
