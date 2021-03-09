3f13a7ffa0d224ddf4832f596a5d853e84f5a4e2
John_Lamy <johnclamy@gmail.com>
2021-03-09 14:59:16 +0000

    added form to pets page

--- src/components/pages/pets.js
+++ src/components/pages/pets.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 import PageContainer from '../layout/PageContainer'
+import AddPet from '../pets/AddPet'
 
 export default function () {
   return (
-    <PageContainer>Pet listings page...</PageContainer>
+    <PageContainer>
+      <header>
+        <h1 className='h2 text-black-50'>Add a pet</h1>
+        <hr />
+      </header>
+      <section>
+        <p className='lead'>Enter the breed of your pup</p>
+        <AddPet />
+      </section>
+    </PageContainer>
   )
 }
--- src/components/pets/AddPet.js
+++ src/components/pets/AddPet.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Button, Form } from 'react-bootstrap'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { addPet } from '../../store/petSlice'
+
 
 export default function () {
+  const dispatch = useDispatch()
+  const [breed, setBreed] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    dispatch(addPet({ id: nanoid(), breed, completed: false }))
+  }
+
   return (
-    <div>add pet form here...</div>
+    <Form onSubmit={handleSubmit}>
+      <Form.Control
+        type='text'
+        size='lg'
+        value={breed}
+        placeholder='Add your pup...'
+        onClick={e => setBreed(e.target.value)}
+      />
+      <Button
+        size='lg'
+        type='submit'
+        className='text-uppercase'
+      >
+        add pet
+      </Button>
+    </Form>
   )
 }
