0c9aaf0a5cd44071cf3eb70f479a6e383222a522
John_Lamy <johnclamy@gmail.com>
2021-03-14 16:50:30 +0000

    refactor component

--- src/components/pages/Pets.js
+++ src/components/pages/Pets.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Dogs from '../../features/Dogs'
+import { DogList } from '../../features/Dogs'
 import { Container, Row, Section } from '../styles'
 import * as ROUTES from '../../routes'
 
@@ -9,7 +9,7 @@ export default function () {
   return (
     <Container>
       <Row>
-        <Dogs />
+        <DogList />
         <Section style={{textAlign: 'center'}} >
           <Link style={{fontSize: '1.1rem'}} to={ROUTES.HOME}>Add a pet</Link>
         </Section>
